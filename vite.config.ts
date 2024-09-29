026d618e27b34496ff389d331e81d3f65fa948c7
taehyun <126179088+KNU-K@users.noreply.github.com>
2024-09-29 20:38:46 +0900

    config : add proxy setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin()],  
+  /**proxy configuration for communicating with spring */
+  server: {
+    open: "/",
+    proxy: {
+        "/api/v1": {
+            target: "http://13.209.21.155", // spring be url
+            changeOrigin: true,
+        },
+    },
+  },
 });
