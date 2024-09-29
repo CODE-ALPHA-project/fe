77cb796d9b71d4fc51a1667a702cbd30ab779d16
kimgho <kgh67811@naver.com>
2024-09-29 12:51:17 +0900

    Fix : config file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { useMediaQuery } from "react-responsive";
 const App = () => {
-  return <h1>asd</h1>;
+  const isMobile = useMediaQuery({ query: "(max-width: 599px)" });
+  const isTablet = useMediaQuery({
+    query: "(min-width: 600px) and (max-width: 1199px)",
+  });
+  const isDesktop = useMediaQuery({ query: "(min-width: 1200px)" });
+  const getBackgroundColor = () => {
+    if (isMobile) {
+      return "lightblue";
+    } else if (isTablet) {
+      return "lightgreen";
+    } else if (isDesktop) {
+      return "lightcoral";
+    }
+  };
+  return (
+    <div style={{ padding: "20px", backgroundColor: getBackgroundColor() }}>
+      <h1>Responsive Design with react-responsive</h1>
+    </div>
+  );
 };
-
 export default App;
