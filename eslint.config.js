0ba723a9c910561b24d93c9b0564ac8d1ab11b96
knu-k <kang4030@knu.ac.kr>
2024-10-04 02:08:46 +0900

    config : convert to react version setting

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 export default [
+  
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
@@ -12,4 +13,12 @@ export default [
   {
     rules: { "react/react-in-jsx-scope": "off", "react/jsx-uses-react": "off" },
   },
+  {
+    // 여기에 settings 추가
+    settings: {
+      react: {
+        version: "detect", // React 버전을 자동으로 감지하도록 설정
+      },
+    },
+  }
 ];
