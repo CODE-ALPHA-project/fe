a2d148ae778047299ea24ab6275f91ff6b5c5750
kimgho <kgh67811@naver.com>
2024-10-09 23:31:28 +0900

    rename : rename file

--- src/components/display/QnASection.tsx
+++ src/components/display/QnASection.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useMediaQuery } from "react-responsive";
-import * as styles from "./QnAsection.css";
+import * as styles from "./qnasection.css";
 import { qnaItems } from "../../mock/qna";
 import { useNavigate } from "react-router-dom";
 
