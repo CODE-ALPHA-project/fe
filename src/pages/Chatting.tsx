a297fc4ab44ccbaf4847403716b36335a4dac22f
kimgho <kgh67811@naver.com>
2024-10-09 23:27:29 +0900

    rename : rename file

--- src/pages/Chatting.tsx
+++ src/pages/Chatting.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import InputBox from "../components/InputBox";
-import { pageContainer, chatArea, inputArea } from "./Chatting.css.ts";
+import { pageContainer, chatArea, inputArea } from "./chatting.css";
 
 // Component
 const ChattingPage = () => {
