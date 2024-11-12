7fb9b1faf2c8fc77e74dbb92ae7b7ab997c0e597
kimgho <kgh67811@naver.com>
2024-11-12 23:51:38 +0900

    build(all): config 수정
    tailwindcss와 shadcn-ui 사용을 위한 패키지 및 config수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,11 @@ export default defineConfig({
         target: "https://13.209.21.155.nip.io", // spring be url
         changeOrigin: true,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   // define:{
