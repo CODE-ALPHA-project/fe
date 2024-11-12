950aa6eef1d7e312b4d73481fceaf388b3660508
kimgho <kgh67811@naver.com>
2024-11-13 00:32:04 +0900

    feat(auth): 로그인/회원가입 작성
    - 로그인 페이지 작성
    - 회원가입 페이지 작성

--- src/pages/Auth/SignIn/SignInPage.tsx
+++ src/pages/Auth/SignIn/SignInPage.tsx
@@ -1,7 +1,82 @@
-import React from "react";
-import * as styles from "../global.css";
+import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { Link } from "react-router-dom";
 
-const SignInPage = () => {
-  return <div className={styles.Container}>Signin</div>;
+const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br p-4">
+      <Link
+        className="w-full max-w-md mb-4 text-black text-center py-2 text-5xl font-bold"
+        to={"/"}
+      >
+        LAWBOT
+      </Link>
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardDescription className="text-center">
+            여러분의 권리를 되찾아보세요
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <div className="relative">
+                  <Input
+                    id="password"
+                    type={showPassword ? "text" : "password"}
+                    required
+                  />
+                  <button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className="absolute right-3 top-1/2 transform -translate-y-1/2"
+                  >
+                    {showPassword ? (
+                      <EyeOffIcon className="h-4 w-4 text-gray-500" />
+                    ) : (
+                      <EyeIcon className="h-4 w-4 text-gray-500" />
+                    )}
+                  </button>
+                </div>
+              </div>
+              <Button className="w-full">로그인</Button>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <p className="text-sm text-gray-500">아직 회원이 아니신가요?</p>
+          <Link className="px-1 text-blue-950 underline" to={"/signup"}>
+            회원가입
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
-export default SignInPage;
+
+export default LoginPage;
