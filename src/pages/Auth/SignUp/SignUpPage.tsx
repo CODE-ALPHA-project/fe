26f8f665b0cdc2f5032f435e82832617ba942084
kimgho <kgh67811@naver.com>
2024-11-13 13:19:40 +0900

    refactor(chat): remove unused line
    - remove unused line in Chatting

--- src/pages/Auth/SignUp/SignUpPage.tsx
+++ src/pages/Auth/SignUp/SignUpPage.tsx
@@ -11,6 +11,7 @@ import { Label } from "@ui/label";
 import { Button } from "@ui/button";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
 import { Link } from "react-router-dom";
+
 const SignupPage = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
