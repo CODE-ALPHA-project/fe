bf67c62f138e71208a99947858cc1ea23898454c
kimgho <kgh67811@naver.com>
2024-11-14 09:42:43 +0900

    feat(payment): add payment page
    - add payment page
    - add payment type
    - modify payment routing path

---
+++ src/pages/Payments/types/types.ts
@@ -0,0 +1,14 @@
+import { Card } from "@/components/ui/card";
+
+export interface PaymentFeature {
+  title: string;
+  description: string;
+}
+
+export interface PaymentPlanProps extends React.ComponentProps<typeof Card> {
+  type: "Basic" | "Premium";
+  title: string;
+  description: string;
+  features: PaymentFeature[];
+  price: number;
+}
