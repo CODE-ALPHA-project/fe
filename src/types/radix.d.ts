cccd4bcde31c861ecf3e6182b41c85eb1ba6f765
kimgho <kgh67811@naver.com>
2024-12-05 01:35:39 +0900

    chore(type): modify type
    - remove duplicated type

--- src/types/radix.d.ts
+++ src/types/radix.d.ts
@@ -1,4 +1,4 @@
-declare module "@radix-ui/react-dialog";
-declare module "@radix-ui/react-separator";
-declare module "@radix-ui/react-slot";
-declare module "@radix-ui/react-tooltip";
+// declare module "@radix-ui/react-dialog";
+// declare module "@radix-ui/react-separator";
+// declare module "@radix-ui/react-slot";
+// declare module "@radix-ui/react-tooltip";
